48123a86dc18d30268aec210df22cb489b22965e
tahasinshadat <107319148+tahasinshadat@users.noreply.github.com>
2024-01-10 14:05:14 -0500

    Create virtual_assistant.py

---
+++ virtual_assistant.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+import pyttsx3
+
+start_phrase = "hey tracer"
+
+class Assistant:
+    # Run the assistant
+    def start_virtual_assistant():
+        print()
+
+    # Access Microphone and get audio file in format of mp3
+    def listen():
+        print()
+
+    # Input audio file and get text
+    def speech_to_text():
+        print()
+
+    # Input text into chatGPT or my own large language model and return response
+    def get_response():
+        print()
+
+    # Input text and repeat the text
+    def text_to_speech():
+        print()
